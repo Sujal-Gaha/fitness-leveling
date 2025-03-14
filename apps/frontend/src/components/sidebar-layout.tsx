a9d5582ceb8a65031b622603423db72a6d9fda2f
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-03-14 10:56:55 +0545

    added a way to open settings modal from the users avatar settings for being convenient

--- apps/frontend/src/components/sidebar-layout.tsx
+++ apps/frontend/src/components/sidebar-layout.tsx
@@ -221,7 +221,7 @@ export const SidebarLayout = ({ children }: { children: ReactNode }) => {
                   <User className="mr-2 h-4 w-4" />
                   Profile
                 </DropdownMenuItem>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={openUserSettingsModal}>
                   <Settings className="mr-2 h-4 w-4" />
                   Settings
                 </DropdownMenuItem>
